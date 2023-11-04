0c7eef29df5dcd1416b6c1c3c5381b8d62b59501
joaobatista235 <juniorbatista0404@gmail.com>
2023-11-04 18:39:32 -0300

    formulario

---
+++ Meu/scripts.js
@@ -0,0 +1,8 @@
+function redirecionar(){
+    let componente = document.getElementById('component');
+    componente.textContent = "AA"
+}
+function limpar(){
+    let componente = document.getElementById('component');
+    componente.textContent = ""
+}
